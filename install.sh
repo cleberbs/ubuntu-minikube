770bc18283cc06e7940041720b187f547efe25e0
cleberbs <cleberbs@gmail.com>
2024-07-08 15:08:37 -0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # Script de instalação das ferramentas para ultilizar o easytrade no docker ou kubernetes
-# ferramentas: -minikube, Docker, kubectl, curl
+# ferramentas: -minikube, Docker, kubectl, git, curl
 
 # Atualizar a lista de pacotes e instalar dependências
 sudo apt update
@@ -15,12 +15,12 @@ sudo chmod a+r /etc/apt/keyrings/docker.asc
 # Adicionar o repositório do Docker
 echo \
   "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/ubuntu \
-  $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | \
+  $(. /etc/os-release && echo \"$VERSION_CODENAME\") stable" | \
   sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
 
 # Atualizar a lista de pacotes e instalar o Docker
 sudo apt update
-sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+sudo apt-get install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
 
 # Adicionar o usuário atual ao grupo Docker
 sudo usermod -aG docker $USER
@@ -30,9 +30,8 @@ curl -LO https://storage.googleapis.com/minikube/releases/latest/minikube-linux-
 sudo install minikube-linux-amd64 /usr/local/bin/minikube && rm minikube-linux-amd64
 
 # Baixar e instalar kubectl
-curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl
+curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl"
 sudo install -o root -g root -m 0755 kubectl /usr/local/bin/kubectl
-
 
 # Verificar as instalações
 echo "Docker version:"
@@ -41,7 +40,7 @@ docker --version
 echo "Docker Compose version:"
 docker-compose --version
 
-echo "kind version:"
+echo "Minikube version:"
 minikube --version
 
 echo "kubectl version:"
